2080a29c8e3abf1aec8816cb16249eca7345aa29
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-07-18 16:05:13 +0800

    add tests for leafnodes
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,12 +1,22 @@
 import unittest
 
 from htmlnode import HTMLNode
+from leafnode import LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_hello_world_with_example_dot_org(self):
         node1 = HTMLNode("a", "Hello, World!", None, {"href": "https://example.com"})
         string_repr = """<a href="https://example.com">Hello, World!</a>"""
-        self.assertEqual(node1.node_to_html(), string_repr)
+        self.assertEqual(node1.to_html(), string_repr)
+
+    def test_leaf_hello_world_with_example_dot_org_with_link_tag(self):
+        leafnode1 = LeafNode("a", "Hello, World!", {"href": "https://example.com"})
+        string_repr = """<a href="https://example.com">Hello, World!</a>"""
+        self.assertEqual(leafnode1.to_html(), string_repr)
+
+    def test_leaf_hello_world_with_example_dot_org_without_a_tag_is_raw_text(self):
+        leafnode1 = LeafNode(None, "Hello, World!", {"href": "https://example.com"})
+        self.assertEqual(leafnode1.to_html(), "Hello, World!")
 
 if __name__ == "__main__":
     unittest.main()
