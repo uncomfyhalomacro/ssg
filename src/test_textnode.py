33ae47a453899cc39caffc80ff53b31f0d47f635
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-07-19 18:12:39 +0800

    add tests for generating all kind of nodes from a node
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -269,6 +269,46 @@ class TestTextNode(unittest.TestCase):
         )
         self.assertEqual([node], new_nodes)
 
+    def test_get_all_nodes(self):
+        node = TextNode(
+            "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        )
+        test_node = [
+            TextNode("This is ", TextType.PLAIN),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.PLAIN),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.PLAIN),
+            TextNode("code block", TextType.INLINE_CODE),
+            TextNode(" and an ", TextType.PLAIN),
+            TextNode(
+                "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
+            ),
+            TextNode(" and a ", TextType.PLAIN),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        self.assertListEqual(node.text_to_textnodes(), test_node)
+
+    def test_get_all_nodes_v2(self):
+        node = TextNode(
+            "This is _italic text_ with an **bold** word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        )
+        test_node = [
+            TextNode("This is ", TextType.PLAIN),
+            TextNode("italic text", TextType.ITALIC),
+            TextNode(" with an ", TextType.PLAIN),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" word and a ", TextType.PLAIN),
+            TextNode("code block", TextType.INLINE_CODE),
+            TextNode(" and an ", TextType.PLAIN),
+            TextNode(
+                "obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"
+            ),
+            TextNode(" and a ", TextType.PLAIN),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        self.assertListEqual(node.text_to_textnodes(), test_node)
+
 
 if __name__ == "__main__":
     unittest.main()
