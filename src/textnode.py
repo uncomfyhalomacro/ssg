711ec2df2dd84c5a709e132aa0b9ceaf2047d391
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-07-19 15:27:35 +0800

    implement getting links and images and also extracting them as nodes.
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- src/textnode.py
+++ src/textnode.py
@@ -58,8 +58,6 @@ class TextNode:
     def md_to_nodes(
         self, delimiter=TextType.PLAIN
     ):  # TODO: Create util functions to process self.extract_markdown_images and self.extract_markdown_links.
-        if self.text_type != TextType.PLAIN:
-            return [self]
         if isinstance(delimiter, str):
             find = list(filter(lambda x: x.value == delimiter, ALL_TEXTYPES_LIST))
             if len(find) == 1:
@@ -67,33 +65,65 @@ class TextNode:
             else:
                 raise ValueError("Error: not a valid delimiter")
 
-        if delimiter == TextType.PLAIN:
+        if not any([d.value == delimiter.value for d in ALL_TEXTYPES_LIST]):
             raise Exception(
                 "Error: no delimiter passed. You can pass the following: **, `, _."
             )
 
         new_nodes = []
         current = self.text
-        if not any([d.value in self.text for d in ALL_TEXTYPES_LIST]):
-            new_node = TextNode(self.text)
-            new_nodes.append(new_node)
-            return new_nodes
-        split_list = current.split(delimiter.value)
-        if len(split_list) > 1 and len(split_list) % 2 == 1:
-            median = len(split_list) // 2
-            for i in range(0, len(split_list)):
-                if i == median:
-                    new_node = TextNode(split_list[i], delimiter)
-                    if new_node not in new_nodes:
-                        new_nodes.append(new_node)
-                else:
-                    new_node = TextNode(split_list[i])
-                    if new_node not in new_nodes:
-                        new_nodes.append(new_node)
-        else:  # This means, the split did not find any pair.
-            raise Exception(
-                f"Error: not valid markdown. There should be another pair of `{delimiter}`"
+        if delimiter in [TextType.LINK, TextType.IMAGE]:
+            # Links and images share similar logic anyway.
+            all_link_tuples = (
+                self.extract_markdown_images()
+                if delimiter == TextType.IMAGE
+                else self.extract_markdown_links()
             )
+            if all_link_tuples == []:  # Just return self inside a list
+                new_nodes.append(self)
+                return new_nodes
+            rejoined_link_as_string = [
+                f"[{x}]({y})" if delimiter == TextType.LINK else f"![{x}]({y})"
+                for (x, y) in all_link_tuples
+            ]
+            initial = 0
+            for idx in range(0, len(rejoined_link_as_string)):
+                found_index = self.text.index(rejoined_link_as_string[idx])
+                plain_text = self.text[initial:found_index]
+                plain_new_node = TextNode(plain_text)
+                linked_new_node = TextNode(
+                    all_link_tuples[idx][0], delimiter, all_link_tuples[idx][1]
+                )
+                new_nodes.append(plain_new_node)
+                new_nodes.append(linked_new_node)
+                initial = found_index + len(rejoined_link_as_string[idx])
+
+            last_text = self.text[initial:]
+            if last_text != "":
+                plain_new_node = TextNode(last_text)
+                new_nodes.append(plain_new_node)
+            return new_nodes
+        else:
+            split_list = current.split(delimiter.value)
+            if len(split_list) == 1:  # No split happened
+                return [self]
+            if len(split_list) > 1 and len(split_list) % 2 == 1:
+                median = len(split_list) // 2
+                for i in range(0, len(split_list)):
+                    if i == median:
+                        new_node = TextNode(split_list[i], delimiter)
+                        if new_node not in new_nodes:
+                            new_nodes.append(new_node)
+                    else:
+                        new_node = TextNode(split_list[i])
+                        if new_node not in new_nodes:
+                            new_nodes.append(new_node)
+            elif (
+                len(split_list) % 2 == 0
+            ):  # This means, the split did not find any pair.
+                raise Exception(
+                    f"Error: not valid markdown. There should be another pair of `{delimiter}`"
+                )
         return new_nodes
 
     def __eq__(self, other):
