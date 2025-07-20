924380c71c5558a1aeeec01695ac15e12cff5c33
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-07-20 18:26:29 +0800

    include split markdown to blocks in tests
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- test.sh
+++ test.sh
@@ -3,3 +3,4 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" || exit ; pwd -P )"
 export SCRIPTPATH
 python3 -m unittest discover -s "$SCRIPTPATH/src"
+python3 -m unittest discover -s "$SCRIPTPATH/src/markdown"
