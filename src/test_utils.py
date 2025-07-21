c14f46a56e590a2424c2d63484bf469d7bbb4c6a
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-07-21 14:05:05 +0800

    add tests for extract title
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

---
+++ src/test_utils.py
@@ -0,0 +1,31 @@
+import unittest
+from src.utils import extract_title
+
+class UtilsTest(unittest.TestCase):
+    def test_extract_title(self):
+        md = """
+# This is a title
+
+WOAH
+"""
+        self.assertEqual("This is a title", extract_title(md))
+
+    def test_fail_no_title(self):
+        md = """
+## This is a title but not a title
+"""
+        with self.assertRaises(Exception):
+            extract_title(md)
+
+    def test_fail_no_content(self):
+        md = """
+
+
+
+
+"""
+        with self.assertRaises(Exception):
+            extract_title(md)
+
+if __name__ == "__main__":
+    unittest.main()
