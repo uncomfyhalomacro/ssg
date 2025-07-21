b0f5e26248a8d30f848063c393d7a25e9a58172e
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-07-21 11:02:02 +0800

    test script now points to just src because we use `__init__.py` now for imports
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- test.sh
+++ test.sh
@@ -3,4 +3,3 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" || exit ; pwd -P )"
 export SCRIPTPATH
 python3 -m unittest discover -s "$SCRIPTPATH/src"
-python3 -m unittest discover -s "$SCRIPTPATH/src/markdown"
