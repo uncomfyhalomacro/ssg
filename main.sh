2d7e37cb038c3076b1ea42231f2c2091511fc1fa
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-07-21 21:18:52 +0800

    run as a module
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- main.sh
+++ main.sh
@@ -2,6 +2,7 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" || exit ; pwd -P )"
 export SCRIPTPATH
-python3 "$SCRIPTPATH/src/main.py"
+cd $SCRIPTPATH
+python3 -m "src"
 cd "$SCRIPTPATH/public" && python3 -m http.server 8888
 
