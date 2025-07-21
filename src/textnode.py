e737a0fe8f0f5c64102a92ae7b590407fb210baf
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-07-21 21:18:30 +0800

    use regexes for inline code blocks, italics, and bold text
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- src/textnode.py
+++ src/textnode.py
@@ -6,11 +6,25 @@ from src.leafnode import LeafNode
 
 class TextType(Enum):
     PLAIN = None
-    BOLD = "**"
-    ITALIC = "_"
-    INLINE_CODE = "`"
+    BOLD = "**"  # r"\*\*(.+?)\*\*"
+    ITALIC = "_"  # r"_(.+?)_"
+    INLINE_CODE = "`"  # r"`(.+?)`"
     IMAGE = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
     LINK = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+
+
+def get_regex_for_pairs(text_type):
+    if text_type not in [TextType.BOLD, TextType.ITALIC, TextType.INLINE_CODE]:
+        raise ValueError(
+            f"Error: passing a value not part of the pairs. Value: `text_type`"
+        )
+    if text_type == TextType.BOLD:
+        return r"\*\*(.+?)\*\*"
+    if text_type == TextType.ITALIC:
+        return r"_(.+?)_"
+    if text_type == TextType.INLINE_CODE:
+        return r"`(.+?)`"
+    raise Exception("Error: Something went wrong in here.")
 
 
 ALL_TEXTYPES_LIST = [
@@ -55,7 +69,7 @@ class TextNode:
         return re.findall(TextType.IMAGE.value, self.text)
 
     def md_to_nodes(
-        self, delimiter=TextType.PLAIN
+        self, delimiter=None
     ):  # TODO: Create util functions to process self.extract_markdown_images and self.extract_markdown_links.
         if isinstance(delimiter, str):
             find = list(filter(lambda x: x.value == delimiter, ALL_TEXTYPES_LIST))
@@ -63,11 +77,6 @@ class TextNode:
                 delimiter = find[0]
             else:
                 raise ValueError("Error: not a valid delimiter")
-
-        if not any([d.value == delimiter.value for d in ALL_TEXTYPES_LIST]):
-            raise Exception(
-                "Error: no delimiter passed. You can pass the following: **, `, _."
-            )
 
         new_nodes = []
         current = self.text
@@ -81,48 +90,54 @@ class TextNode:
             if all_link_tuples == []:  # Just return self inside a list
                 new_nodes.append(self)
                 return new_nodes
-            rejoined_link_as_string = [
+            rejoined_link_as_strings = [
                 f"[{x}]({y})" if delimiter == TextType.LINK else f"![{x}]({y})"
                 for (x, y) in all_link_tuples
             ]
             initial = 0
-            for idx in range(0, len(rejoined_link_as_string)):
-                found_index = self.text.index(rejoined_link_as_string[idx])
-                plain_text = self.text[initial:found_index]
+            for idx in range(0, len(rejoined_link_as_strings)):
+                found_index = current.index(rejoined_link_as_strings[idx])
+                plain_text = current[initial:found_index]
                 plain_new_node = TextNode(plain_text)
                 linked_new_node = TextNode(
                     all_link_tuples[idx][0], delimiter, all_link_tuples[idx][1]
                 )
                 new_nodes.append(plain_new_node)
                 new_nodes.append(linked_new_node)
-                initial = found_index + len(rejoined_link_as_string[idx])
+                initial = found_index + len(rejoined_link_as_strings[idx])
 
-            last_text = self.text[initial:]
+            last_text = current[initial:]
             if last_text != "":
                 plain_new_node = TextNode(last_text)
                 new_nodes.append(plain_new_node)
-            return new_nodes
-        else:
-            split_list = current.split(delimiter.value)
-            if len(split_list) == 1:  # No split happened
-                return [self]
-            if len(split_list) > 1 and len(split_list) % 2 == 1:
-                median = len(split_list) // 2
-                for i in range(0, len(split_list)):
-                    if i == median:
-                        new_node = TextNode(split_list[i], delimiter)
-                        if new_node not in new_nodes:
-                            new_nodes.append(new_node)
-                    else:
-                        new_node = TextNode(split_list[i])
-                        if new_node not in new_nodes:
-                            new_nodes.append(new_node)
-            elif (
-                len(split_list) % 2 == 0
-            ):  # This means, the split did not find any pair.
-                raise Exception(
-                    f"Error: not valid markdown. There should be another pair of `{delimiter}`"
+        elif delimiter in [TextType.BOLD, TextType.ITALIC, TextType.INLINE_CODE]:
+            regex = get_regex_for_pairs(delimiter)
+            rejoined_paired_strings = [
+                f"{delimiter.value}{f}{delimiter.value}"
+                for f in re.findall(regex, current)
+            ]
+            initial = 0
+            for idx in range(0, len(rejoined_paired_strings)):
+                found_index = current.index(rejoined_paired_strings[idx])
+                plain_text = current[initial:found_index]
+                plain_new_node = TextNode(plain_text)
+                special_paired_node = TextNode(
+                    rejoined_paired_strings[idx].strip(delimiter.value), delimiter, None
                 )
+                new_nodes.append(plain_new_node)
+                new_nodes.append(special_paired_node)
+                initial = found_index + len(rejoined_paired_strings[idx])
+
+            last_text = current[initial:]
+            if last_text != "":
+                plain_new_node = TextNode(last_text)
+                new_nodes.append(plain_new_node)
+        if delimiter is None:
+            raise Exception(
+                "Error: passing a value not part of the pairs. Value: `text_type`"
+                != "Error: no delimiter passed. You can pass the following: **, `, _."
+            )
+
         return new_nodes
 
     def text_to_textnodes(self):
@@ -136,12 +151,23 @@ class TextNode:
                 all_indices_tuple.append((a_match.start(), delimiter))
         all_indices_tuple = sorted(all_indices_tuple, key=lambda item: item[0])
         for idx, text_type in all_indices_tuple:
-            # print(idx, text_type)
             nodes = self.md_to_nodes(text_type)
-            for node in nodes:
-                # print("TEST NODE: ", node)
-                if all(
-                    [d.value not in node.text for d in ALL_TEXTYPES_LIST[:3]]
+            while nodes:
+                node = nodes.pop(0)
+                if any(
+                    [node.text.count(d.value) > 1 for d in ALL_TEXTYPES_LIST[:3]]
+                ) or any(
+                    [
+                        len(re.findall(d.value, node.text)) > 0
+                        for d in ALL_TEXTYPES_LIST[3:]
+                    ]
+                ):
+                    temp_nodes = node.text_to_textnodes()
+                    for tnode in temp_nodes:
+                        if tnode not in new_nodes:
+                            new_nodes.append(tnode)
+                elif all(
+                    [node.text.count(d.value) <= 1 for d in ALL_TEXTYPES_LIST[:3]]
                 ) and all(
                     [
                         len(re.findall(d.value, node.text)) == 0
@@ -150,11 +176,6 @@ class TextNode:
                 ):
                     if node not in new_nodes:
                         new_nodes.append(node)
-                else:
-                    temp_nodes = node.text_to_textnodes()
-                    for tnode in temp_nodes:
-                        if tnode not in new_nodes:
-                            new_nodes.append(tnode)
         if new_nodes == []:
             return [self]
         return new_nodes
